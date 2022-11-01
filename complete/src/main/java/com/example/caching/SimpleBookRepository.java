acd817e81fbdecfb70c95fc623480c121cb36ee3
David McReynolds <david.mcreynolds@gmail.com>
2022-11-01 00:15:20 -0400

    logging

--- complete/src/main/java/com/example/caching/SimpleBookRepository.java
+++ complete/src/main/java/com/example/caching/SimpleBookRepository.java
@@ -1,16 +1,20 @@
 package com.example.caching;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleBookRepository implements BookRepository {
-
+	private static final Logger logger = LoggerFactory.getLogger(SimpleBookRepository.class);
 	@Override
 	@Cacheable("books")
 	public Book getByIsbn(String isbn) {
 		simulateSlowService();
-		return new Book(isbn, "Some book");
+		Book rVal = new Book(isbn, "Some book");
+		logger.info("pulled {}", rVal.toString());
+		return rVal;
 	}
 
 	// Don't do this at home
